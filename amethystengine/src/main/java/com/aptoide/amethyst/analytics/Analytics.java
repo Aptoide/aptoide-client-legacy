70bad402d00d8c02a0bb1e0110204b9149925666
Frederico Santinho <fredericosantinho@gmail.com>
2016-01-22 19:40:27 +0000

    Adapted Analytics.java to work with Aptoide Logger.

--- amethystengine/src/main/java/com/aptoide/amethyst/analytics/Analytics.java
+++ amethystengine/src/main/java/com/aptoide/amethyst/analytics/Analytics.java
@@ -11,6 +11,7 @@ import com.aptoide.amethyst.R;
 import com.aptoide.amethyst.downloadmanager.model.Download;
 import com.aptoide.amethyst.preferences.EnumPreferences;
 import com.aptoide.amethyst.utils.AptoideUtils;
+import com.aptoide.amethyst.utils.Logger;
 import com.aptoide.amethyst.webservices.json.GetApkInfoJson;
 import com.aptoide.dataprovider.webservices.models.v7.GetApp;
 import com.flurry.android.FlurryAgent;
@@ -26,7 +27,6 @@ import java.util.HashMap;
 public class Analytics {
 
     private static final boolean ACTIVATE = true;
-    private static final boolean DEBUG = false;
 
     private static final int ALL = Integer.MAX_VALUE;
     private static final int LOCALYTICS = 1 << 0;
@@ -57,9 +57,8 @@ public class Analytics {
 
             track(event, stringObjectHashMap, flags);
 
-            if (DEBUG) {
-                System.out.println("Debug: Analytics: Event: " + event + ", Key: " + key + ", attr: " + attr);
-            }
+            Logger.d("Analytics", "Event: " + event + ", Key: " + key + ", attr: " + attr);
+
         } catch (Exception e) {
             Log.d("Analytics", e.getStackTrace().toString());
         }
@@ -74,9 +73,7 @@ public class Analytics {
             if(checkAcceptability(flags, LOCALYTICS))
                 Localytics.tagEvent(event, map);
 
-            if (DEBUG) {
-                System.out.println("Debug: Analytics: Event: " + event + ", Map: " + map);
-            }
+            Logger.d("Analytics", "Event: " + event + ", Map: " + map);
 
         } catch (Exception e) {
             Log.d("Analytics", e.getStackTrace().toString());
@@ -92,9 +89,8 @@ public class Analytics {
             if(checkAcceptability(flags, LOCALYTICS))
                 Localytics.tagEvent(event);
 
-            if (DEBUG) {
-                System.out.println("Debug: Analytics: Event: " + event);
-            }
+            Logger.d("Analytics", "Event: " + event);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -156,10 +152,9 @@ public class Analytics {
 
                 Localytics.handleTestMode(activity.getIntent());
 
-                if (DEBUG) {
-                    System.out.println("Debug: Analytics: Event: CPU_ID: " + cpuid);
-                    System.out.println("Debug: Analytics: Screen: " + screenName);
-                }
+                Logger.d("Analytics", "Event: CPU_ID: " + cpuid);
+                Logger.d("Analytics", "Screen: " + screenName);
+
             }
 
             public static void onPause(android.app.Activity activity) {
@@ -200,9 +195,7 @@ public class Analytics {
             if (!ACTIVATE)
                 return;
 
-            if (DEBUG) {
-                System.out.println("Debug: Analytics: Localytics: Screens: " + screenName);
-            }
+            Logger.d("Analytics", "Localytics: Screens: " + screenName);
 
             Localytics.tagScreen(screenName);
             Localytics.upload();
@@ -767,9 +760,7 @@ public class Analytics {
                 return;
             }
 
-            if (DEBUG) {
-                System.out.println("Debug: Analytics: Dimension: " + i + ", Value: " + s);
-            }
+            Logger.d("Analytics", "Dimension: " + i + ", Value: " + s);
 
             Localytics.setCustomDimension(i, s);
         }
@@ -817,9 +808,7 @@ public class Analytics {
 
                 map.put("packageName", packageName);
 
-                if (DEBUG) {
-                    System.out.println("Debug: Analytics: LTV: " + eventName + ": " + packageName + ", " + value);
-                }
+                Logger.d("Analytics", "LTV: " + eventName + ": " + packageName + ", " + value);
 
                 Localytics.tagEvent(eventName, map, value);
             } catch (NumberFormatException e) {
