cb73af7ba7f889fbbe4c79a7669173f6328d13af
Marcelo Benites <marcelo.benites@aptoide.com>
2016-05-12 16:22:12 -0300

    use real sixpack url and credentials for release builds

--- amethystengine/src/main/java/com/aptoide/amethyst/analytics/ABTestManager.java
+++ amethystengine/src/main/java/com/aptoide/amethyst/analytics/ABTestManager.java
@@ -10,13 +10,18 @@ import com.seatgeek.sixpack.Alternative;
 import com.seatgeek.sixpack.Sixpack;
 import com.seatgeek.sixpack.SixpackBuilder;
 import com.seatgeek.sixpack.log.LogLevel;
+import com.squareup.okhttp.Credentials;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import okhttp3.Authenticator;
 import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Route;
 
 public class ABTestManager {
 
@@ -25,19 +30,36 @@ public class ABTestManager {
 	private static ABTestManager instance;
 
 	private SixpackBuilder sixpackBuilder;
+	private final OkHttpClient httpClient;
+	private final String sixpackUrl;
 	private final ExecutorService executorService;
 	private final Set<ABTest<?>> tests;
 	private Sixpack sixpack;
 
 	public static ABTestManager getInstance() {
 		if (instance == null) {
-			instance = new ABTestManager(new SixpackBuilder(), Executors.newCachedThreadPool());
+			instance = new ABTestManager(new SixpackBuilder(), new OkHttpClient.Builder()
+					.authenticator(new Authenticator() {
+
+				@Override
+				public okhttp3.Request authenticate(Route route, okhttp3.Response response) throws
+						IOException {
+					return response.request()
+							.newBuilder()
+							.header("Authorization", Credentials.basic(BuildConfig.SIXPACK_USER,
+									BuildConfig.SIXPACK_PASSWORD))
+							.build();
+				}
+			}).build(), BuildConfig.SIXPACK_URL, Executors.newCachedThreadPool());
 		}
 		return instance;
 	}
 
-	private ABTestManager(SixpackBuilder sixpackBuilder, ExecutorService executorService) {
+	private ABTestManager(SixpackBuilder sixpackBuilder, OkHttpClient httpClient, String
+			sixpackUrl, ExecutorService executorService) {
 		this.sixpackBuilder = sixpackBuilder;
+		this.httpClient = httpClient;
+		this.sixpackUrl = sixpackUrl;
 		this.executorService = executorService;
 		this.tests = new HashSet<>();
 	}
@@ -49,7 +71,8 @@ public class ABTestManager {
 	}
 
 	private void initializeSixpack(String clientId) {
-		sixpack = sixpackBuilder.setSixpackUrl(HttpUrl.parse("http://10.0.2.2:5000/sixpack/"))
+		sixpack = sixpackBuilder.setSixpackUrl(HttpUrl.parse(sixpackUrl))
+				.setHttpClient(httpClient)
 				.setClientId(clientId)
 				.setLogLevel(BuildConfig.DEBUG ? LogLevel.VERBOSE : LogLevel.NONE)
 				.build();
@@ -67,7 +90,7 @@ public class ABTestManager {
 		executorService.submit(new Runnable() {
 			@Override
 			public void run() {
-				for (ABTest test: tests) {
+				for (ABTest test : tests) {
 					test.prefetch();
 				}
 			}
@@ -76,8 +99,8 @@ public class ABTestManager {
 
 	@SuppressWarnings("unchecked")
 	public <T> ABTest<T> get(String name) {
-		for (ABTest test: tests) {
-			if (test.getName().equals(name)){
+		for (ABTest test : tests) {
+			if (test.getName().equals(name)) {
 				return (ABTest<T>) test;
 			}
 		}
