559e9fd4b97179ae497288b19ad1bcee6b5572c7
Wanghong Lin <wanghong@Wanghongs-MacBook-Pro.local>
2016-08-26 19:07:34 +0800

    format html tag in suggested app's description

--- amethystengine/src/main/java/com/aptoide/amethyst/AppViewMiddleSuggested.java
+++ amethystengine/src/main/java/com/aptoide/amethyst/AppViewMiddleSuggested.java
@@ -2,6 +2,7 @@ package com.aptoide.amethyst;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -23,7 +24,7 @@ import com.aptoide.amethyst.downloadmanager.model.Download;
 import com.aptoide.amethyst.utils.AptoideUtils;
 import com.aptoide.amethyst.utils.Logger;
 import com.aptoide.amethyst.utils.ReferrerUtils;
-import com.aptoide.amethyst.utils.SimpleFuture;
+import com.aptoide.amethyst.webservices.GetApkInfoRequestFromId;
 import com.aptoide.amethyst.webservices.json.GetApkInfoJson;
 import com.aptoide.amethyst.webservices.v2.GetAdsRequest;
 import com.aptoide.models.ApkSuggestionJson;
@@ -35,8 +36,6 @@ import com.octo.android.robospice.request.listener.RequestListener;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.aptoide.amethyst.webservices.GetApkInfoRequestFromId;
 
 import static com.aptoide.dataprovider.webservices.models.Constants.AD_ID_KEY;
 import static com.aptoide.dataprovider.webservices.models.Constants.APPNAME_KEY;
@@ -207,7 +206,7 @@ public class AppViewMiddleSuggested {
 
                     ((TextView) view.findViewById(R.id.app_name)).setText(name);
                     ((TextView) view.findViewById(R.id.size_value)).setText(df.format(size1) + " MB");
-                    ((TextView) view.findViewById(R.id.description)).setText(description1);
+                    ((TextView) view.findViewById(R.id.description)).setText(Html.fromHtml(description1));
                     ((RatingBar) view.findViewById(R.id.rating_label)).setRating(rating);
 
                     ImageView imageView = (ImageView) view.findViewById(R.id.app_icon);
