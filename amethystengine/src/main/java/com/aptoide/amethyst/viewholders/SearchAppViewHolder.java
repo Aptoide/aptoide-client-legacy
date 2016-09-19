3a1e35cf9e3eb4c3d2c652234a3b5150598584ce
pedroribeiro <ribasss@gmail.com>
2016-09-19 17:28:45 +0100

    Code fix.

--- amethystengine/src/main/java/com/aptoide/amethyst/viewholders/SearchAppViewHolder.java
+++ amethystengine/src/main/java/com/aptoide/amethyst/viewholders/SearchAppViewHolder.java
@@ -153,12 +153,9 @@ public class SearchAppViewHolder extends BaseViewHolder {
 
         store.setText(appItem.getRepo());
         Glide.with(itemView.getContext()).load(AptoideUtils.UI.parseIcon(appItem.getIcon())).into(icon);
-
-            store.setText(appItem.getRepo());
         } else {
             store.setVisibility(View.INVISIBLE);
         }
-        Glide.with(itemView.getContext()).load(AptoideUtils.UI.parseIcon(appItem.getIcon())).into(icon);
 
         if (appItem.getMalwareRank() == 2) {
             icTrusted.setVisibility(View.VISIBLE);
